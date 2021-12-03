bd96e97dd6fb8f2307d7da734d942a71dc16d42a
Rhett Winston <rhettius@gmail.com>
2021-12-03 21:25:14 +1100

    day1

---
+++ day1/day1-2/day1-2.cpp
@@ -0,0 +1,73 @@
+// day1-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> read_data()
+{
+	int cur{ 0 };
+	vector<int> data;
+	while (cin >> cur)
+	{
+		data.push_back(cur);
+	}
+	return data;
+}
+
+vector<int> calc_windows(const vector<int>& data)
+{
+	vector<int> windows;
+	for(int i = 0; i < data.size(); i++)
+	{
+		if (i + 2 < data.size())
+		{
+			windows.push_back(data[i] + data[i + 1] + data[i + 2]);
+		}
+	}
+	return windows;
+}
+
+int count_increased(const vector<int>& data)
+{
+	int prev{ 0 };
+	int increased{ 0 };
+	bool has_prev = false;
+	for (int cur : data)
+	{
+		if (has_prev)
+		{
+			if (cur > prev)
+			{
+				++increased;
+			}
+		}
+
+		has_prev = true;
+		prev = cur;
+	}
+	return increased;
+}
+
+int main()
+{
+	vector<int> data = read_data();
+	vector<int> windows = calc_windows(data);
+
+	cout << count_increased(windows) << " increased windows.";
+	
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

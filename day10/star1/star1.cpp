c6ef9632422d43f5356c9ae7676f400af6998566
Rhett Winston <rhettius@gmail.com>
2021-12-12 12:37:04 +1100

    Day 10 star 1 done.

--- day10/star1/star1.cpp
+++ day10/star1/star1.cpp
@@ -1,20 +1,85 @@
-// star1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+bool isOpenBracket(char c)
+{
+	return
+		c == '[' ||
+		c == '{' ||
+		c == '(' ||
+		c == '<';
+}
+
+char getCloseFromOpenBracket(char c)
+{
+	if (c == '[')
+		return ']';
+	if (c == '{')
+		return '}';
+	if (c == '(')
+		return ')';
+	if (c == '<')
+		return '>';
+	return 0;
+}
+
+int getPoints(char c)
+{
+	switch (c)
+	{
+	case ')':
+		return 3;
+	case ']':
+		return 57;
+	case '}':
+		return 1197;
+	case '>':
+		return 25137;
+	default: 
+		return 0;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string line;
+
+	int filePoints = 0;
+	while (cin >> line && line.length() > 0)
+	{
+		int linePoints = 0;
+		stack<char> openBrackets;
+		for (char c : line)
+		{
+			if (isOpenBracket(c))
+			{
+				openBrackets.push(c);
+			}
+			else
+			{
+				if (openBrackets.size() > 0 && 
+					getCloseFromOpenBracket(openBrackets.top()) == c) //Then the close bracket matches the open one, all good. 
+				{
+					openBrackets.pop();
+				}
+				else //syntax error. 
+				{
+					cout << "Expected " << ((openBrackets.size() > 0) ? getCloseFromOpenBracket(openBrackets.top()) : '?') <<
+						", but found " << c << " instead. " << getPoints(c) <<" points." << endl;
+					linePoints += getPoints(c);
+					break;
+				}
+			}
+		}
+
+		filePoints += linePoints;
+	}
+
+	cout << "Total file points: " << filePoints;
+    
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

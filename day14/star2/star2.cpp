b2dc76c3ff0f8c8d2478600376aca1a2f4a4f7e1
Rhett Winston <rhettius@gmail.com>
2021-12-22 19:49:15 +1100

    Day 14 star 2 fixed.

--- day14/star2/star2.cpp
+++ day14/star2/star2.cpp
@@ -5,76 +5,82 @@
 
 using namespace std;
 
-
-char getFirst(uint16_t charPair)
-{
-	return static_cast<char>(charPair);
-}
-
-char getSecond(uint16_t charPair)
-{
-	return static_cast<char>(charPair >> 8);
-}
-
-uint16_t getCharPair(char A, char B)
-{
-	return (((uint16_t)A) << 8) + ((uint16_t)B);
-}
+typedef pair<char, char> poly;
 
 class processing
 {
-	const map<uint16_t, char>& rules;
+	const map<poly, char>& rules;
 	string templ;
-	map<char, uint64_t> counts;
+	map<char, uint64_t> counts; 
+	map<poly, uint64_t> curPolyCounts;
+	map<poly, uint64_t> nextPolyCounts; 
 
 public:
 
-	processing(const map<uint16_t, char>& rules, string templ) : rules(rules), templ(templ)
+	void initialiseCounts()
 	{
 		for (char c = 'A'; c <= 'Z'; c++)
 		{
 			counts[c] = 0;
 		}
+	}
 
-		//Each char is counted in the initial map. 
-		for (int i = 0; i < templ.length(); i++)
+	void countInitialPolyChars()
+	{
+		for (unsigned int i = 0; i < templ.length(); i++)
 		{
 			char curItem = templ.substr(i, 1)[0];
 			counts[curItem]++;
 		}
 	}
-	
-	void go()
+
+	void initialisePolyCounts()
 	{
-		//expand each initial pair in the template.
-		for (int i = 0; i < templ.length() - 1; i++)
+		for (unsigned int i = 0; i < templ.length() - 1; i++)
 		{
-			string curItem = templ.substr(i, 2);
-			cout << "Starting initial pair: " << curItem << endl;
-			expand(curItem[0], curItem[1], 0);
+			string curPair = templ.substr(i, 2);
+			poly p{ curPair[0] , curPair[1] };
+			addToPolyCount(curPolyCounts, p, 1);
 		}
 	}
 
-	void expand(char A, char B, int depth)
+	processing(const map<poly, char>& rules, string templ) : rules(rules), templ(templ)
 	{
-		static uint64_t count = 0;
-		if (depth < 40)
-		{
-			auto r = rules.find(getCharPair(A, B));
-			if (r != rules.end())
-			{
-				char insertingChar = r->second;
-				counts[insertingChar]++;
+		initialiseCounts();
+		countInitialPolyChars();
+		initialisePolyCounts();
+	}
+	
+	void addToPolyCount(map<poly, uint64_t>& polycounts, poly p, uint64_t amt)
+	{
+		if (polycounts.count(p) == 0)
+			polycounts.insert({ p, amt });
+		else
+			polycounts[p]+= amt;
+	}
 
-				expand(A, insertingChar, depth + 1);
-				expand(insertingChar, B, depth + 1);
-			}
-		}
-		count++;
-		if (count % 100000000 == 0)
+	void go()
+	{
+		for (int level = 1; level <= 40; level++)
 		{
-			cout << count/1000000 << "M" <<  endl;
+			cout << "Starting level " << level << endl;
+			for (auto currentPoly : curPolyCounts)
+			{
+				auto matchingRule = rules.find(currentPoly.first);
+				if (matchingRule != rules.end())
+				{
+					counts[matchingRule->second] += currentPoly.second;
+					poly polyL { currentPoly.first.first, matchingRule->second };
+					poly polyR{ matchingRule->second, currentPoly.first.second };
+					addToPolyCount(nextPolyCounts, polyL, currentPoly.second);
+					addToPolyCount(nextPolyCounts, polyR, currentPoly.second);
+				}
+			}
+
+			curPolyCounts = nextPolyCounts;
+			nextPolyCounts.clear();
 		}
+
 	}
 
 	void printResults()
@@ -82,11 +88,14 @@ public:
 		uint64_t largest = 0, smallest = UINT64_MAX;
 		for (pair<char, uint64_t> item : counts)
 		{
-			cout << "Letter " << item.first << " count: " << item.second << endl;
-			if (item.second < smallest && item.second > 0)  //have to exclude the zeroed-out alphabet as some letters are never included.
-				smallest = item.second;
-			if (item.second > largest)
-				largest = item.second;
+			if (item.second > 0)
+			{
+				cout << "Letter " << item.first << " count: " << item.second << endl;
+				if (item.second < smallest) 
+					smallest = item.second;
+				if (item.second > largest)
+					largest = item.second;
+			}
 		}
 		cout << "Largest minus smallest = " << (largest - smallest) << endl;
 	}
@@ -94,19 +103,18 @@ public:
 };
 
 
-
 int main()
 {
 	string templ;
 	cin >> templ;
 
-	map<uint16_t, char> rules;
+	map<poly, char> rules;
 
 	string ruleL, ruleR, spacer;
 
 	while (cin >> ruleL && cin >> spacer && cin >> ruleR)
 	{
-		rules.insert({ getCharPair(ruleL[0], ruleL[1]) , ruleR[0] });
+		rules.insert({ poly(ruleL[0], ruleL[1]) , ruleR[0] });
 	}
 
 	processing p(rules, templ);

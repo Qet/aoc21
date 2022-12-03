93792edd52f6c124e0da2845bf40d68cbeef0bb2
Rhett Winston <rhettius@gmail.com>
2022-12-04 09:00:25 +1100

    Day 2 star 2

---
+++ day22/star2/star2.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <tuple>
+#include <vector>
+#include <set>
+#include <sstream>
+
+using namespace std;
+
+typedef tuple<int, int, int> coord;
+typedef pair<int, int> range;
+
+pair<int, int> rangeFromString(string data, int from, int to)
+{
+	int dd = data.find("..", from);
+	return pair<int, int>{
+		stoi(data.substr(from, dd - from)),
+			stoi(data.substr(dd + 2, to - dd - 2))
+	};
+}
+
+bool isOutsideInitRange(range r)
+{
+	return
+		(r.first < -50 && r.second < -50) ||
+		(r.first > 50 && r.second > 50);
+}
+
+int main()
+{
+	set<coord> oncubes;
+	string onoff;
+	string cubeoid;
+
+	while (cin >> onoff && cin >> cubeoid)
+	{
+		cout << "Processing: " << cubeoid << endl;
+		int xpos = cubeoid.find("x=");
+		int ypos = cubeoid.find(",y=");
+		int zpos = cubeoid.find(",z=");
+		bool isOn = onoff == "on";
+
+		pair<int, int> x = rangeFromString(cubeoid, xpos + 2, ypos);
+		pair<int, int> y = rangeFromString(cubeoid, ypos + 3, zpos);
+		pair<int, int> z = rangeFromString(cubeoid, zpos + 3, cubeoid.length());
+
+		if (!(isOutsideInitRange(x) || isOutsideInitRange(y) || isOutsideInitRange(z)))
+		{
+			for (int i = x.first; i <= x.second; i++)
+			{
+				for (int j = y.first; j <= y.second; j++)
+				{
+					for (int k = z.first; k <= z.second; k++)
+					{
+						if (isOn)
+						{
+							oncubes.insert(coord{ i,j,k });
+						}
+						else
+						{
+							oncubes.erase(coord{ i,j,k });
+						}
+					}
+				}
+			}
+		}
+	}
+
+	cout << "On cubes: " << oncubes.size() << endl;
+}
+
+
+// coordinates: +x east, +y north, +z up. 
+// find the z range of each cubeoid and note whether they're an on or off type.
+// find the total z range of all cubeoids. 
+// move layer by layer from the top to the bottom, calculating how many squares are "on" on each layer. 
+// a cubeoid is "active" if the current z value is within its z range. only consider the active cuboids. 
